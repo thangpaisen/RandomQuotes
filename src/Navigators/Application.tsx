c9ca4ddb011184d4857605a63500b2de5530938a
lavanyaRamesh064 <lavanyaramesh2903@gmail.com>
2023-11-17 13:36:07 +0530

    push changes

--- src/Navigators/Application.tsx
+++ src/Navigators/Application.tsx
@@ -3,6 +3,7 @@ import { HomeScreen, LoginScreen, SplashScreen } from '@/Navigators/Stack'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
+
 import { SafeAreaView, StatusBar, StyleSheet } from 'react-native'
 import MainBottomTab from './MainBottomTab'
 import { navigationRef } from './utils'
