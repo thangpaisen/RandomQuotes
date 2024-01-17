ce91a760b6023d2f35b8b9be4c721eac458185ef
yogeshwar-chaudhari-77 <chaudhari.yogesh20@gmail.com>
2024-01-17 22:07:50 +1030

    general cleanup, removed unused screens and code

--- src/Navigators/Application.tsx
+++ src/Navigators/Application.tsx
@@ -1,16 +1,8 @@
-import { Colors, Texts } from '@/Constants'
-import { HomeScreen } from '@/Navigators/Stack'
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import React from 'react'
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native'
 import { navigationRef } from './utils'
-import RandomQuoteScreen from '@/Containers/RandomQuote/RandomQuoteScreen'
-import AuthorScreen from '@/Containers/Author/AuthorScreen'
-import TagsScreen from '@/Containers/TagsScreen/TagsScreen'
-import TagsDetailScreen from '@/Containers/TagsDetailScreen/TagsDetailScreen'
 import QuoteByAuthorScreen from '@/Containers/QuoteByAuthor/QuoteByAuthorScreen'
-import QuoteDetailsScreen from '@/Containers/QuoteDetail/QuoteDetailsScreen'
 import { MainBottomNavigation } from './MainBottomNavigation'
 
 const Stack = createStackNavigator()
@@ -19,13 +11,8 @@ const ApplicationNavigator = () => {
   return (
     <NavigationContainer ref={navigationRef}>
       <MainBottomNavigation />
-      {/* <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={Texts.HomeScreen}>
-        <Stack.Screen name={Texts.RandomQuoteScreen} component={RandomQuoteScreen} />
-        <Stack.Screen name={Texts.AuthorScreen} component={AuthorScreen} />
-        <Stack.Screen name={Texts.TagsScreen} component={TagsScreen} />
-        <Stack.Screen name={Texts.TagsDetailScreen} component={TagsDetailScreen} />
-        <Stack.Screen name={Texts.QuoteByAuthorScreen} component={QuoteByAuthorScreen} />
-        <Stack.Screen name={Texts.QuoteDetailsScreen} component={QuoteDetailsScreen} />
+      {/* <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name={'QuoteByAuthorScreen'} component={QuoteByAuthorScreen} />
       </Stack.Navigator> */}
     </NavigationContainer>
   )
--- src/Navigators/Stack.ts
+++
@@ -1,4 +0,0 @@
-import ProfileScreen from '@/Containers/Profile/ProfileScreen'
-import { HomeScreen } from '@/Containers/Home/HomeScreen'
-
-export { ProfileScreen, HomeScreen }
